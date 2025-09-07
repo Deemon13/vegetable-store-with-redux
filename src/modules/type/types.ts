cd508ef77a40dbdc0b42f41103f04ad1b0f3e617
Dmitriy Fedorenko <dmitriy.fedorenko13@gmail.com>
2025-09-07 18:18:56 +0300

    test: добавляет немного тестов

--- src/modules/type/types.ts
+++ src/modules/type/types.ts
@@ -3,10 +3,7 @@ export interface VegetableType {
   name: string;
   price: number;
   image?: string;
-  // category?: string;
-  // amount?: number;
 }
 export interface CartVegetableType extends VegetableType {
-  // item: VegetableType;
   amount: number;
 }
